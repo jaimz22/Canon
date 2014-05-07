9d63c26c71752d9a774d8117d502b2506c78df59
James Murray <james.murray@boxgroove.com>
2014-05-07 13:22:27 -0400

    Added necessary methods to timestamp trait

--- Traits/Timestamps.php
+++ Traits/Timestamps.php
@@ -50,4 +50,51 @@ trait Timestamps
 		$this->updatedAt = new \DateTime();
 		return $this;
 	}
+
+
+	/**
+	 * Set createdAt
+	 *
+	 * @param \DateTime $createdAt
+	 * @return mixed
+	 */
+	public function setCreatedAt($createdAt)
+	{
+		$this->createdAt = $createdAt;
+
+		return $this;
+	}
+
+	/**
+	 * Get createdAt
+	 *
+	 * @return \DateTime
+	 */
+	public function getCreatedAt()
+	{
+		return $this->createdAt;
+	}
+
+	/**
+	 * Set updatedAt
+	 *
+	 * @param \DateTime $updatedAt
+	 * @return mixed
+	 */
+	public function setUpdatedAt($updatedAt)
+	{
+		$this->updatedAt = $updatedAt;
+
+		return $this;
+	}
+
+	/**
+	 * Get updatedAt
+	 *
+	 * @return \DateTime
+	 */
+	public function getUpdatedAt()
+	{
+		return $this->updatedAt;
+	}
 } 
